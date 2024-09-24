6d78fee2254662b75e7ae1cd2cbfe2f5f6ced162
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-09-24 13:11:30 +0200

    add functions to update city and temperature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temp-celsius");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchACity(city) {
+  let apiKey = "21f1caaedf3t0e238fc991db4d2f34bo";
+  let encodedCity = encodeURIComponent(city);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodedCity}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function enterACity(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   let city = cityInputElement.value;
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = city;
+  searchACity(cityInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", enterACity);
+
+searchACity("London");
